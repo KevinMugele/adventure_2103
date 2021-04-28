f958a7cba830e45bb9e81df6829daa55d307e75c
Suzie K <suziekim.dev@gmail.com>
2021-04-28 11:20:04 -0400

    Add test for initialize method

---
+++ spec/hiker_spec.rb
@@ -0,0 +1,21 @@
+require 'rspec'
+require './lib/hiker'
+require './lib/park'
+require './lib/trail'
+
+describe Hiker do
+  describe '::initialize' do
+    it 'exists' do
+      hiker = Hiker.new('Dora', :moderate)
+
+      expect(hiker).is_a? Hiker 
+    end
+    
+    it 'has attributes' do
+      hiker = Hiker.new('Dora', :moderate)
+      
+      expect(hiker.name).to eq 'Dora'
+      expect(hiker.experience_level).to eq :moderate
+    end 
+  end
+end
