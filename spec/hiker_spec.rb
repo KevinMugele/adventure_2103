a29e1064e87b008d61a020ff04f24cfedfde5fbd
Suzie K <suziekim.dev@gmail.com>
2021-04-28 11:24:35 -0400

    Add test for pack method

--- spec/hiker_spec.rb
+++ spec/hiker_spec.rb
@@ -19,4 +19,18 @@ describe Hiker do
       expect(hiker.snacks).to eq ({})
     end 
   end
+
+  describe '#pack' do
+    it 'adds snack and snack quantity to snacks hash' do
+      hiker = Hiker.new('Dora', :moderate)
+      hiker.pack('water', 1)
+      hiker.pack('trail mix', 3)
+
+      expect(hiker.snacks).to eq ({"water"=>1, "trail mix"=>3})
+
+      hiker.pack('water', 1)
+
+      expect(hiker.snacks).to eq ({"water"=>2, "trail mix"=>3})
+    end
+  end
 end
