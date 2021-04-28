90ca4b28c653b2f404f1b91f813ed3606d8a9a97
Suzie K <suziekim.dev@gmail.com>
2021-04-28 11:08:24 -0400

    Complete and test initialize method

--- spec/trail_spec.rb
+++ spec/trail_spec.rb
@@ -13,8 +13,8 @@ describe Trail do
       trail1 = Trail.new({name: 'Grand Wash', length: '2.2 miles', level: :easy})
 
       expect(trail1.name).to eq "Grand Wash"
-      expect(trail1.length).to eq 2.2
-      expect(trail1.length).to eq :easy
+      expect(trail1.length).to eq '2.2 miles'
+      expect(trail1.level).to eq :easy
     end 
   end
 end
